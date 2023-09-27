60a4f2bb5eaea0003ae6ccfd4df2608b5fde5791
fseda <felipeseda@poli.ufrj.br>
2023-09-27 12:57:24 -0300

    Load config file with godotenv

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"os"
+	"strings"
 
 	"github.com/fseda/cookbooked-api/internal/infra/config"
 	"github.com/fseda/cookbooked-api/internal/infra/database"
 	"github.com/fseda/cookbooked-api/pkg/shutdown"
+	"github.com/joho/godotenv"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/log"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/helmet"
+	"github.com/gofiber/fiber/v2/middleware/idempotency"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
@@ -17,8 +21,12 @@ func main() {
 	// Exit code for graceful shutdown
 	var exitCode int
 	defer func() { os.Exit(exitCode) }()
-
+	
 	// If not able to get env, app logs Fatal error
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Warn("Could not load environment variables")
+	}
 	env := config.NewConfig()
 
 	cleanup, err := run(env)
@@ -57,8 +65,33 @@ func buildServer(env *config.Config) (*fiber.App, func(), error) {
 
 	app := fiber.New(fiber.Config{})
 
-	app.Use(cors.New())
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowMethods: strings.Join([]string{
+			fiber.MethodGet,
+			fiber.MethodPost,
+			fiber.MethodDelete,
+			fiber.MethodPut,
+			fiber.MethodPatch,
+			fiber.MethodHead,
+		}, ","),
+	}))
 	app.Use(logger.New())
+	app.Use(helmet.New())
+	app.Use(idempotency.New())
+
+	app.Hooks().OnListen(func(listenData fiber.ListenData) error {
+		if fiber.IsChild() {
+			return nil
+		}
+		scheme := "http"
+		if listenData.TLS {
+			scheme = "https"
+		}
+
+		log.Infof("🚀 Server listening on %v://%v:%v", scheme, listenData.Host, listenData.Port)
+		return nil
+	})
 
 	app.Get("/health", func(c *fiber.Ctx) error {
 		return c.SendString("Healthy!")

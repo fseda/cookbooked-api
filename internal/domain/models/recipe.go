10317f7e5726f2d29b01aaa7cad49f1216e69d85
fseda <felipeseda@poli.ufrj.br>
2023-09-27 00:26:02 -0300

    Feat: Finish database model

--- internal/domain/models/recipe.go
+++ internal/domain/models/recipe.go
@@ -6,20 +6,33 @@ import (
 
 type Recipe struct {
 	gorm.Model
-	ID          uint   `gorm:"column:id; primaryKey; autoIncrement; not null;" json:"id"`
-	Title       string `gorm:"column:title; size:255; not null; uniqueIndex:unique_user_id_title;" json:"title"`
-	Description string `gorm:"column:description; not null;" json:"description"`
-	Body        string `gorm:"column:body; not null;" json:"body"`
-	Link        string `gorm:"column:link; size:500; not null;" json:"link"`
-	UserID      uint   `gorm:"column:user_id; uniqueIndex:unique_user_id_title;" json:"user_id"`
-	RecipeTags  []RecipeTag
+	ID                uint               `gorm:"column:id; primaryKey; autoIncrement; not null;" json:"id"`
+	Title             string             `gorm:"column:title; size:255; not null; uniqueIndex:unique_user_id_title;" json:"title"`
+	Description       string             `gorm:"column:description; not null;" json:"description"`
+	Body              string             `gorm:"column:body; not null;" json:"body"`
+	Link              string             `gorm:"column:link; size:500; not null;" json:"link"`
+	UserID            uint               `gorm:"column:user_id; uniqueIndex:unique_user_id_title;" json:"user_id"`
+	RecipeTags        []RecipeTag        `gorm:"constraint:OnDelete:CASCADE"`
+	RecipeIngredients []RecipeIngredient `gorm:"constraint:OnDelete:RESTRICT"`
 }
 
 type RecipeTag struct {
 	gorm.Model
 	ID       uint   `gorm:"column:id; primaryKey; autoIncrement; not null;" json:"id"`
-	RecipeID uint   `gorm:"column:recipe_id; uniqueIndex: recipe_tags_must_be_unique;" json:"recipe_id"`
+	RecipeID uint   `gorm:"column:recipe_id; uniqueIndex:recipe_tags_must_be_unique;" json:"recipe_id"`
 	Recipe   Recipe `gorm:"foreignKey:RecipeID; references:ID"`
-	TagID    uint   `gorm:"column:tag_id; uniqueIndex: recipe_tags_must_be_unique;" json:"tag_id"`
+	TagID    uint   `gorm:"column:tag_id; uniqueIndex:recipe_tags_must_be_unique;" json:"tag_id"`
 	Tag      Tag    `gorm:"foreignKey:TagID; references:ID"`
+}
+
+type RecipeIngredient struct {
+	gorm.Model
+	ID           uint       `gorm:"column:id; primaryKey; autoIncrement; not null;" json:"id"`
+	Quantity     float32    `gorm:"column:quantity; not null;" json:"quantity"`
+	RecipeID     uint       `gorm:"column:recipe_id; uniqueIndex:recipe_ingredients_must_be_unique;" json:"recipe_id"`
+	Recipe       Recipe     `gorm:"foreignKey:RecipeID; references:ID"`
+	IngredientID uint       `gorm:"column:ingredient_id; uniqueIndex:recipe_ingredients_must_be_unique;" json:"ingredient_id"`
+	Ingredient   Ingredient `gorm:"foreignKey:IngredientID; references:ID"`
+	UnitID       uint       `gorm:"column:unit_id;" json:"unit_id"`
+	Unit         Unit       `gorm:"foreignKey:UnitID; references:ID"`
 }
--- internal/domain/models/tag.go
+++ internal/domain/models/tag.go
@@ -6,9 +6,9 @@ import (
 
 type Tag struct {
 	gorm.Model
-	ID          uint   `gorm:"column:id; primaryKey; autoIncrement; not null;" json:"id"`
-	Name        string `gorm:"column:name; size:100; not null; uniqueIndex:user_tags_must_be_unique" json:"name"`
-	IsSystemTag bool   `gorm:"column:is_system_tag; default:false; not null; uniqueIndex:user_tags_must_be_unique" json:"is_system_tag"`
-	UserID      uint   `gorm:"column:user_id; uniqueIndex:user_tags_must_be_unique" json:"user_id"`
-	RecipeTags  []RecipeTag
+	ID          uint        `gorm:"column:id; primaryKey; autoIncrement; not null;" json:"id"`
+	Name        string      `gorm:"column:name; size:100; not null; uniqueIndex:user_tags_must_be_unique" json:"name"`
+	IsSystemTag bool        `gorm:"column:is_system_tag; default:false; not null; uniqueIndex:user_tags_must_be_unique" json:"is_system_tag"`
+	UserID      uint        `gorm:"column:user_id; uniqueIndex:user_tags_must_be_unique" json:"user_id"`
+	RecipeTags  []RecipeTag `gorm:"constraint:OnDelete:CASCADE"`
 }

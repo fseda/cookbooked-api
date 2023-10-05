e5aaa0ad1d957ba39fa3a184fd2ea4dbe24516bd
fseda <felipeseda@poli.ufrj.br>
2023-10-05 01:17:18 -0300

    Add custom 'role' type for database interaction

--- internal/domain/models/user.go
+++ internal/domain/models/user.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"database/sql/driver"
+
 	"gorm.io/gorm"
 )
 
@@ -9,8 +11,25 @@ type User struct {
 	Username     string       `gorm:"column:username; size:255; unique; not null;" json:"username"`
 	Email        string       `gorm:"column:email; size:255; unique; not null;" json:"email"`
 	PasswordHash string       `gorm:"column:password_hash; not null;" json:"-"`
+	Role         role         `gorm:"column:role; type:ENUM('user', 'admin'); default:'user'; not null;" json:"role"`
 	Recipes      []Recipe     `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL" json:"recipes"`
 	Tags         []Tag        `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL" json:"tags"`
 	Ingredients  []Ingredient `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL" json:"ingredients"`
 	Units        []Unit       `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL" json:"units"`
+}
+
+type role string
+
+const (
+	ADMIN role = "admin"
+	USER  role = "user"
+)
+
+func (r *role) Scan(value interface{}) error {
+	*r = role(value.([]byte))
+	return nil
+}
+
+func (r role) Value() (driver.Value, error) {
+	return string(r), nil
 }

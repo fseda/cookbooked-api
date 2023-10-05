c8d242ff842a0871cae3ed6f6f2e89653dbcd06c
fseda <felipeseda@poli.ufrj.br>
2023-10-04 23:57:54 -0300

    Fix typo and exclude passwordHash from json

--- internal/domain/models/user.go
+++ internal/domain/models/user.go
@@ -6,13 +6,11 @@ import (
 
 type User struct {
 	gorm.Model
-	Username     string       `gorm:"column:username; size:255; unique; not null;" json:"userame"`
+	Username     string       `gorm:"column:username; size:255; unique; not null;" json:"username"`
 	Email        string       `gorm:"column:email; size:255; unique; not null;" json:"email"`
-	PasswordHash string       `gorm:"column:password_hash; not null;" json:"passwordhash"`
+	PasswordHash string       `gorm:"column:password_hash; not null;" json:"-"`
 	Recipes      []Recipe     `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL" json:"recipes"`
 	Tags         []Tag        `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL" json:"tags"`
 	Ingredients  []Ingredient `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL" json:"ingredients"`
 	Units        []Unit       `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL" json:"units"`
 }
-
-

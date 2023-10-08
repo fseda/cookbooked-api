1bb06fe89f787e727e9b1b12bc9094be429fccfe
fseda <felipeseda@poli.ufrj.br>
2023-10-08 14:21:35 -0300

    Add error handling for unregistered routes

--- internal/infra/httpapi/routes/router.go
+++ internal/infra/httpapi/routes/router.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"github.com/fseda/cookbooked-api/internal/infra/config"
+	"github.com/fseda/cookbooked-api/internal/infra/httpapi/httpstatus"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -13,4 +14,7 @@ func LoadRoutes(ctx *config.AppContext) {
 	// app.Get("/swagger", swagger.HandlerDefault)
 	loadUserRoutes(ctx.App, ctx.DB)
 	loadAuthRoutes(ctx.App, ctx.DB, ctx.Env)
+	ctx.App.All("*", func(c *fiber.Ctx) error {
+		return httpstatus.NotFoundError("Not Found")
+	})
 }

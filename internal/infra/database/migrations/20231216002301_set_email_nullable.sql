af21479aef1a3bd7bf02660301700398127a3792
fseda <felipeseda@poli.ufrj.br>
2024-01-04 20:29:06 -0300

    set email nullable

---
+++ internal/infra/database/migrations/20231216002301_set_email_nullable.sql
@@ -0,0 +1,10 @@
+-- +goose Up
+-- +goose StatementBegin
+  ALTER TABLE users ALTER COLUMN email DROP NOT NULL;
+
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+  ALTER TABLE users ALTER COLUMN email SET NOT NULL;
+-- +goose StatementEnd
